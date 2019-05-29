e580062e395badb0f268302a0a136720c5afa734
faizalmp <faizalpriyowibowo@gmail.com>
2019-05-29 22:17:31 +0700

    update view

--- view/src/components/Mitra.vue
+++ view/src/components/Mitra.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="657">
+    <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="100%">
     <div>
         <v-layout align-center column>
             <v-flex>
@@ -9,12 +9,29 @@
                 <v-layout grid-list column>
                 <v-form>
                     <v-text-field label="Nomor Resi" solo v-model="init.no_resi"></v-text-field>
-                    <v-text-field label="Nomor Resi" solo v-model="init.nama_mitra"></v-text-field>
-                    <v-text-field label="Nomor Resi" solo v-model="init.nama_barang"></v-text-field>
-                    <v-text-field label="Nomor Resi" solo v-model="init.nama_pengirim"></v-text-field>
-                    <v-text-field label="Nomor Resi" solo v-model="init.nama_penerima"></v-text-field>
-                    
+                    <v-text-field label="Mitra" solo v-model="init.nama_mitra"></v-text-field>
+                    <v-text-field label="Barang" solo v-model="init.nama_barang"></v-text-field>
+                    <v-text-field label="pengirim" solo v-model="init.nama_pengirim"></v-text-field>
+                    <v-text-field label="penerima" solo v-model="init.nama_penerima"></v-text-field>
+                    <v-subheader class="headline">Asal</v-subheader>
+                    <v-text-field label="Kode Pos" solo v-model="lokasi_asal.kodepos"></v-text-field>
+                    <v-text-field label="Kecamatan" solo v-model="lokasi_asal.kecamatan"></v-text-field>
+                    <v-text-field label="Kota" solo v-model="lokasi_asal.kota"></v-text-field>
+                    <v-text-field label="Provinsi" solo v-model="lokasi_asal.provinsi"></v-text-field>
+                    <v-subheader class="headline">Tujuan</v-subheader>
+                    <v-text-field label="Kode Pos" solo v-model="lokasi_tujuan.kodepos"></v-text-field>
+                    <v-text-field label="Kecamatan" solo v-model="lokasi_tujuan.kecamatan"></v-text-field>
+                    <v-text-field label="Kota" solo v-model="lokasi_tujuan.kota"></v-text-field>
+                    <v-text-field label="Provinsi" solo v-model="lokasi_tujuan.provinsi"></v-text-field>
+                    <v-btn @click="add" dark color="primary">Buat resi</v-btn>
                 </v-form>
+                    <v-subheader class="headline">UPDATE LOKASI</v-subheader>
+                    <v-text-field label="Nomor Resi" solo v-model="updateresi"></v-text-field>
+                    <v-text-field label="Kode Pos" solo v-model="lokasi_sekarang.kodepos"></v-text-field>
+                    <v-text-field label="Kecamatan" solo v-model="lokasi_sekarang.kecamatan"></v-text-field>
+                    <v-text-field label="Kota" solo v-model="lokasi_sekarang.kota"></v-text-field>
+                    <v-text-field label="Provinsi" solo v-model="lokasi_sekarang.provinsi"></v-text-field>
+                    <v-btn @click="update" dark color="primary">UPDATE</v-btn>
                 </v-layout>
             </v-flex>
         </v-layout>
@@ -26,25 +43,64 @@
 import axios from 'axios'
 export default {
     data: () => ({
+        updateresi: '',
         init: {
             no_resi: '',
             nama_mitra: '',
             nama_barang: '',
             nama_pengirim: '',
             nama_penerima: '',
-            lokasi_asal: {
-                kecamatan: '',
-                kota: '',
-                provinsi: ''
-            },
-            lokasi_tujuan: {
-                kecamatan: '',
-                kota: '',
-                provinsi: ''
-            },
+            
             tanggal_kirim: 0,
 
+        },
+        from: '',
+        now: '',
+        to: '',
+        lokasi_asal: {
+                kode_pos: '',
+                kecamatan: '',
+                kota: '',
+                provinsi: ''
+            },
+        lokasi_sekarang: {
+            kode_pos: '',
+                kecamatan: '',
+                kota: '',
+                provinsi: ''
+            },
+        lokasi_tujuan: {
+            kode_pos: '',
+                kecamatan: '',
+                kota: '',
+                provinsi: ''
+            }
+    }),
+    methods: {
+        add(){
+            const params = new URLSearchParams();
+          params.append('no_resi', this.init.no_resi);
+          params.append('nama_mitra', this.init.nama_mitra);
+          params.append('nama_barang', this.init.nama_barang);
+          params.append('lokasi_asal', this.lokasi_asal);
+          params.append('lokasi_tujuan', this.lokasi_tujuan);
+          params.append('tanggal_kirim', this.init.tanggal_kirim);
+            axios.post('http://trexin.herokuapp.com/api/resi/', params
+        ).then(response => {
+          console.log(response)
+          })
+          .catch(error => console.log(JSON.stringify(error)))
+        },
+        update(){
+            const params = new URLSearchParams();
+            params.append('no_resi', this.updateresi);
+            params.append('lokasi_sekarang', this.lokasi_sekarang);
+            axios.post('http://trexin.herokuapp.com/api/resi/' + this.updateresi, params
+        ).then(response => {
+          console.log(response)
+          })
+          .catch(error => console.log(JSON.stringify(error)))
         }
-    })
+    }
 }
 </script>
--- view/src/components/User.vue
+++ view/src/components/User.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="657">
+  <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="100%">
   <div>
     <v-layout justify-center align-center column>
       <v-flex>
@@ -10,19 +10,40 @@
             <v-btn @click="initialize" color="primary">Cari Resi</v-btn>
           </v-form>
         </v-flex>
-        <v-flex mt-4>
-          <v-data-table :headers="headers" :items="items" :search="search" clipped-left hide-actions class="elevation-1">
+        <v-flex mt-4 v-if="resi != null">
+          <!--<span>{{ resi }}</span>-->
+          <!--<v-data-table :headers="headers" :items="resi" clipped-left hide-actions class="elevation-1">
             <template v-slot:items="props">
               <td class="text-xs-left">{{ props.item.no_resi }}</td>
               <td class="text-xs-left">{{ props.item.nama_mitra }}</td>
               <td class="text-xs-left">{{ props.item.nama_barang }}</td>
               <td class="text-xs-left">{{ props.item.nama_pengirim }}</td>
               <td class="text-xs-left">{{ props.item.nama_penerima }}</td>
-              <td class="text-xs-left">{{ props.item.from }}</td>
-              <td class="text-xs-left">{{ props.item.now }}</td>
-              <td class="text-xs-left">{{ props.item.to }}</td>
+              <td class="text-xs-left">{{ props.from }}</td>
+              <td class="text-xs-left">{{ props.now }}</td>
+              <td class="text-xs-left">{{ props.to }}</td>
             </template>
-          </v-data-table>
+          </v-data-table>-->
+
+
+
+              <v-text-field dark outline readonly v-model="resifix.no_resi" label="Nomor Resi"></v-text-field>
+
+              <v-text-field dark outline readonly v-model="resifix.nama_mitra" label="Mitra"></v-text-field>
+
+              <v-text-field dark outline readonly v-model="resifix.nama_barang" label="Barang"></v-text-field>
+
+              <v-text-field dark outline readonly v-model="resifix.nama_pengirim" label="Pengirim"></v-text-field>
+
+              <v-text-field dark outline readonly v-model="resifix.nama_penerima" label="Penerima"></v-text-field>
+
+              <v-text-field dark outline readonly v-model="from" label="Dari"></v-text-field>
+
+              <v-text-field dark outline readonly v-model="now" label="Sekarang"></v-text-field>
+
+              <v-text-field dark outline readonly v-model="to" label="Tujuan"></v-text-field>
+
+          <img :src="iklan">
         </v-flex>
       </v-flex>
     </v-layout>
@@ -32,46 +53,73 @@
 
 <script>
 import axios from 'axios'
+import { setInterval } from 'timers';
   export default {
-    data: () => ({
+    data:  function(){
+      return {
       cari_resi: '',
-      items: [],
+      resi: null,
       from: '',
       now: '',
       to: '',
-      tanggalkirim: '',
+      iklan: '',
+      resifix:{
+        no_resi: '',
+        nama_mitra: '',
+        nama_barang: '',
+        nama_pengirim:'',
+        nama_penerima: '',
+        from: '',
+        now: '',
+        to: '',
+      },
+      tanggalkirim: 0,
       headers: [
-          { text: 'Nomor Resi', align: 'left', value: 'no_resi'},
-          { text: 'Mitra', align: 'left', value: 'nama_mitra' },
-          { text: 'Paket Barang', align: 'left', value: 'nama_barang' },
-          { text: 'Pengirim', align: 'left', value: 'nama_pengirim' },
-          { text: 'Penerima', align: 'left', value: 'nama_penerima' },
-          { text: 'Asal', align: 'left', value: from },
-          { text: 'Sekarang', align: 'left', value: now },
-          { text: 'Tujuan', align: 'left', value: to },
-          { text: 'Tanggal Pengiriman', align: 'left', value: tanggalkirim }
+          { text: 'Nomor Resi', align: 'left'},
+          { text: 'Mitra', align: 'left'},
+          { text: 'Paket Barang', align: 'left'},
+          { text: 'Pengirim', align: 'left' },
+          { text: 'Penerima', align: 'left' },
+          { text: 'Asal', align: 'left' },
+          { text: 'Sekarang', align: 'left'},
+          { text: 'Tujuan', align: 'left' },
+          { text: 'Tanggal Pengiriman', align: 'left' }
         ]
-    }),
-    watch: {
-      dialog (val) {
-        val || this.close()
-      }
+    }
     },
     created () {
       this.initialize()
+      this.getIklan();
+      setInterval(() => {
+        this.getIklan();
+      }, 15000);
     },
     methods: {
       initialize () {
         console.log(this.cari_resi)
         axios.get('http://trexin.herokuapp.com/api/resi/' + this.cari_resi).then(response => {
-              this.items = response.data_resi
+              this.resi = response.data.data_resi
               this.from = this.resi.lokasi_asal.kecamatan + ", " + this.resi.lokasi_asal.kota + ", " + this.resi.lokasi_asal.provinsi
               this.now = this.resi.lokasi_sekarang.kecamatan + ", " + this.resi.lokasi_sekarang.kota + ", " + this.resi.lokasi_sekarang.provinsi
               this.to = this.resi.lokasi_tujuan.kecamatan + ", " + this.resi.lokasi_tujuan.kota + ", " + this.resi.lokasi_tujuan.provinsi
-              console.log(items)
-          }).catch(error => console.log(error)) 
-       
+              this.resifix.no_resi = this.resi.no_resi
+              this.resifix.nama_mitra = this.resi.nama_mitra
+              this.resifix.nama_barang = this.resi.nama_barang
+              this.resifix.nama_pengirim = this.resi.nama_pengirim
+              this.resifix.nama_penerima = this.resi.nama_penerima 
+              this.resifix.from = this.from
+              this.resifix.now = this.now
+              this.resifix.to = this.to  
+              console.log(this.resi)
+              
+          }).catch(error => console.log(error))        
       },
+      getIklan: function(){
+        axios.get('https://arta.ruangkarya.id/service/ads/img-ads').then(response => {
+          this.iklan = response.data.data.url
+          console.log(response)
+        }).catch(error => console.log(error))
+      }
 
     }
   }
